ec7ec688005522b7df245703d18d47e1f5e37013
Colz-code <Rofiganteng245@gmail.com>
2026-08-01 19:17:24 +0700

    Update Game list.lua

--- Game list.lua
+++ Game list.lua
@@ -1,5 +1,6 @@
 local Games = {
-  [100572791490444] = "https://raw.githubusercontent.com/Colz-code/Colz-hub/refs/heads/main/Layangan", -- layangan
+  [100572791490444] = "https://raw.githubusercontent.com/Colz-code/Colz-hub/refs/heads/main/Layangan", -- Layangan
+  
 }
 
 return Games
